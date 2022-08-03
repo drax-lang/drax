c797453434ceb542d3d74e6e43b620ee4e5e18cc
jeantux <jeaanca.santos@gmail.com>
2022-08-03 18:46:37 -0300

    Added drax const definitions

---
+++ src/ddefs.h
@@ -0,0 +1,16 @@
+/* drax Lang - 2022
+ * Jean Carlos (jeantux)
+ */
+ 
+#ifndef __DDEFS
+#define __DDEFS
+
+#define UNUSED(x) (void)(x)
+
+#define MAX_INSTRUCTIONS 80
+
+#define STACK_OP_SIZE   256
+
+#define MAX_STACK_SIZE (MAX_INSTRUCTIONS * STACK_OP_SIZE)
+
+#endif
