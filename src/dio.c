1007585ecdd960a6957d62e050542ae18557bcac
JeanTux <jeaanca.santos@gmail.com>
2023-02-24 22:32:25 -0300

    Added build for windows

--- src/dio.c
+++ src/dio.c
@@ -5,15 +5,24 @@
 #include "dio.h"
 
 char* b_pwd() {
+  #ifdef _WIN32
+
+  #include <direct.h>
+  #define MAX_PATH_SIZE 255
+
+  char* relative_path = (char*) malloc(sizeof(char) * MAX_FATH_SIZE);
+  _getcwd(relative_path, MAX_PATH_SIZE);
+  return relative_path;
+  #else
   long size;
   char *relative_path;
 
   size = pathconf("./", _PC_PATH_MAX);
-
   if ((relative_path = (char *) malloc((size_t) size)) != NULL)
       getcwd(relative_path, (size_t) size);
 
   return relative_path;
+  #endif
 }
 
 char* normalize_path(char* path) {
@@ -23,12 +32,12 @@ char* normalize_path(char* path) {
   size_t rs = strlen(r_path);
   size_t cs = strlen(path);
   char* full_path = (char*) malloc(sizeof(char) * (rs + cs + 2));
-  
+ 
   for (size_t i = 0; i < rs; i++) { full_path[i] = r_path[i]; }
   full_path[rs] = '/';
   for (size_t i = 0; i < cs; i++) { full_path[i + rs + 1] = path[i]; }
   full_path[rs + cs + 1] = '\0';
-  
+ 
   return full_path;
 }
 
