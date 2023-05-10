5eb1f48ec6073bc2d6b5c04d82e465cbd250cbe6
JeanTux <jeaanca.santos@gmail.com>
2023-05-09 21:46:10 -0300

    removed _exit

--- src/mods/d_mod_os.c
+++ src/mods/d_mod_os.c
@@ -29,75 +29,75 @@ char* replace_special_char(char special, char new, char* str) {
 
 #if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 199309L
 int d_command(const char* command, char* output, int output_size) {
-    FILE *pipe = popen(command, "r");
-    int status;
+  FILE *pipe = popen(command, "r");
+  int status;
 
-    if (pipe == NULL) {
-      printf("Fail to create pipe\n");
-      return -1;
+  if (pipe == NULL) {
+    printf("Fail to create pipe\n");
+    return -1;
+  }
+
+  int total_bytes_read = 0;
+  char buf[BUF_SIZE];
+
+  while (fgets(buf, sizeof(buf), pipe) != NULL) {
+    int bytes_to_copy = strlen(buf);
+    if (total_bytes_read + bytes_to_copy >= output_size) {
+        printf("Output buffer too small\n");
+        pclose(pipe);
+        return -1;
     }
 
-    int total_bytes_read = 0;
-    char buf[BUF_SIZE];
+    memcpy(output + total_bytes_read, buf, bytes_to_copy);
+    total_bytes_read += bytes_to_copy;
+  }
+  output[total_bytes_read] = '\0';
+  status = pclose(pipe);
 
-    while (fgets(buf, sizeof(buf), pipe) != NULL) {
-      int bytes_to_copy = strlen(buf);
-      if (total_bytes_read + bytes_to_copy >= output_size) {
-          printf("Output buffer too small\n");
-          pclose(pipe);
-          return -1;
-      }
-
-      memcpy(output + total_bytes_read, buf, bytes_to_copy);
-      total_bytes_read += bytes_to_copy;
-    }
-    output[total_bytes_read] = '\0';
-    status = pclose(pipe);
-
-    return WEXITSTATUS(status);
+  return WEXITSTATUS(status);
 }
 #else
 int d_command(const char* command, char* output, int output_size) {
-    int pipe_fd[2];
-    pid_t pid;
+  int pipe_fd[2];
+  pid_t pid;
 
-    if (pipe(pipe_fd) == -1) {
-      return -1;
-    }
+  if (pipe(pipe_fd) == -1) {
+    return -1;
+  }
 
-    pid = fork();
-    if (pid == -1) {
-      close(pipe_fd[0]);
-      close(pipe_fd[1]);
-      return -1;
-    }
+  pid = fork();
+  if (pid == -1) {
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+    return -1;
+  }
 
-    if (pid == 0) {
-      close(pipe_fd[0]);
-      dup2(pipe_fd[1], STDOUT_FILENO);
-      close(pipe_fd[1]);
-      execl("/bin/sh", "sh", "-c", command, NULL);
-      _exit(127);
-    } else {
-      close(pipe_fd[1]);
-      int total_bytes_read = 0;
-      int bytes_read;
-      char buf[BUF_SIZE];
+  if (pid == 0) {
+    close(pipe_fd[0]);
+    dup2(pipe_fd[1], STDOUT_FILENO);
+    close(pipe_fd[1]);
+    execl("/bin/sh", "sh", "-c", command, NULL);
+    return -1;
+  } else {
+    close(pipe_fd[1]);
+    int total_bytes_read = 0;
+    int bytes_read;
+    char buf[BUF_SIZE];
 
-      while ((bytes_read = read(pipe_fd[0], buf, BUF_SIZE)) > 0) {
-        if (total_bytes_read + bytes_read > output_size) {
-          errno = EOVERFLOW;
-          return -1;
-        }
-        memcpy(output + total_bytes_read, buf, bytes_read);
-        total_bytes_read += bytes_read;
+    while ((bytes_read = read(pipe_fd[0], buf, BUF_SIZE)) > 0) {
+      if (total_bytes_read + bytes_read > output_size) {
+        errno = EOVERFLOW;
+        return -1;
       }
-      output[total_bytes_read] = '\0';
-
-      int status;
-      waitpid(pid, &status, 0);
-
-      return WEXITSTATUS(status);
+      memcpy(output + total_bytes_read, buf, bytes_read);
+      total_bytes_read += bytes_read;
     }
+    output[total_bytes_read] = '\0';
+
+    int status;
+    waitpid(pid, &status, 0);
+
+    return WEXITSTATUS(status);
+  }
 }
 #endif
